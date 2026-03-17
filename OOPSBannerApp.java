da47a50f1ec47acbf89649019b3b089195726926
sreehari326 <sreeeharisuraj26@gmail.com>
2026-03-18 02:25:42 +0530

    UC6 - Banner using static methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,51 @@
 public class OOPSBannerApp {
 
+    public static String[] getOPattern() {
+        return new String[]{
+            "  ***  ",
+            " *   * ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " *   * ",
+            "  ***  "
+        };
+    }
+
+    // P pattern
+    public static String[] getPPattern() {
+        return new String[]{
+            " ***** ",
+            " *   * ",
+            " *   * ",
+            " ***** ",
+            " *     ",
+            " *     ",
+            " *     "
+        };
+    }
+
+    // S pattern
+    public static String[] getSPattern() {
+        return new String[]{
+            " ***** ",
+            " *     ",
+            " *     ",
+            " ***** ",
+            "     * ",
+            " *   * ",
+            " ***** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] lines = {
-            String.join(" ", "  ***  ", "  ***  ", " ***** ", " ***** "),
-            String.join(" ", " *   * ", " *   * ", " *   * ", " *     "),
-            String.join(" ", "*     *", "*     *", " *   * ", " *     "),
-            String.join(" ", "*     *", "*     *", " ***** ", " ***** "),
-            String.join(" ", "*     *", "*     *", " *     ", "     * "),
-            String.join(" ", " *   * ", " *   * ", " *     ", " *   * "),
-            String.join(" ", "  ***  ", "  ***  ", " *     ", " ***** ")
-        };
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + " " + o[i] + " " + p[i] + " " + s[i]);
         }
-
     }
 }
